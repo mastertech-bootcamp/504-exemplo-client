624aaeaf0b6f5c9616f7d7e731f9e06c1fa4d557
Bruno Pimentel <brunoapimentel@gmail.com>
2018-02-09 09:41:47 -0200

    Listagem e cadastro de clientes

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  dados = {}
 
-  constructor(public navCtrl: NavController) {
+  constructor(public http: HttpClient) {
 
+  }
+
+  enviar(){
+    this.http.post('http://45.55.53.12:3000/clientes', this.dados).subscribe(() => {
+      alert('Os dados foram salvos!');
+    });
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  cadastros;
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+    this.http.get('http://45.55.53.12:3000/clientes').subscribe(
+      (dados) => {
+        this.cadastros = dados;
+        console.log(dados);
+        
+      }
+    )
   }
 
 }
